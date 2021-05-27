77fbe3b6a972bc0de6b38f54e1ef0ab6560b54fe
chen <2305211762@qq.com>
2021-05-27 10:11:25 +0800

    第一次提交

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,21 @@
 <template>
-    <div>
-      <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+<!--      头部区域-->
+          <el-header>
+            <div>
+                <img src="" alt="">
+                <span>电商后台管理系统</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+          </el-header>
+<!--      页面主体区域-->
+          <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+          </el-container>
+      </el-container>
+
+
 </template>
 
 <script>
@@ -17,5 +31,17 @@ export default {
 </script>
 
 <style scoped>
+  .el-header{
+    background-color: #373d41;
+  }
+  .el-aside{
+    background-color: #333744;
+  }
+  .el-main{
+    background-color: #eaedf1;
+  }
+  .home-container{
+    height: 100%;
+  }
 
 </style>
